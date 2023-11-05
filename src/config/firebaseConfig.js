242adabb3ea4f919855b1ce646076d445fc57e6c
Ashwin Talwalkar <atalwalkar719@gmail.com>
2023-11-05 14:47:54 -0600

    moved secrets to env

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbWFseVt-d5eBjRMhvolLKha1jKO20rJM",
-  authDomain: "renew-wisconsin-402101.firebaseapp.com",
-  projectId: "renew-wisconsin-402101",
-  storageBucket: "renew-wisconsin-402101.appspot.com",
-  messagingSenderId: "492981142944",
-  appId: "1:492981142944:web:0e7448fe502d2bad2310c5",
-  measurementId: "G-6BJH4YRDXM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
